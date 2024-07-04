0b98a77be50c2e71b5026f13b3f41b32582b3b77
Korol <kib23-a.korol@nubip.edu.ua>
2024-07-04 12:24:21 +0300

    commit

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int n, count = 0;
+    printf("Введіть натуральне число n: ");
+    scanf("%d", &n);
+
+    for (int m = 1; m < n; m++) {
+        if (n / m == n % m) {
+            count++;
+        }
+    }
+
+    printf("Кількість рівних дільників числа %d: %d\n", n, count);
+    return 0;
+}
